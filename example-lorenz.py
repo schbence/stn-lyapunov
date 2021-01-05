ecb7d139d5f393d512b54583831e75d7c8d1565f
Bence Schneider <bence.schneider@geogebra.at>
2021-01-06 00:33:40 +0200

    correction

--- example-lorenz.py
+++ example-lorenz.py
@@ -38,7 +38,7 @@ def multi_measure(dir, rhos, trials, meas, attrib=None):
     return M
 
 if __name__=='__main__':
-    rhos = arange(181.620, 181.750, 0.001)
+    rhos = np.arange(181.620, 181.750, 0.001)
     trials = np.arange(10)
     lyaps = multi_measure('./lorenz_stns/', rhos, trials, lyapunov_parallel_one_path, attrib='weight')
     lyaps_avgd = lyaps.mean(axis=1)
